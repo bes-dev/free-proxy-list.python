1e1e60043a713472bf2664f6f28cc09d1a7e7eff
Сергей Белоусов <s.belousov@aiby.com>
2024-05-19 15:12:00 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# free-proxy-list.python
-Simple python client for https://free-proxy-list.net/
+# free-proxy-list client
+
+Simple python client to https://free-proxy-list.net/
+
+
+## Requirements
+
+* Linux, Windows, MacOS
+* Python 3.8.+
+
+## Install package
+
+```bash
+pip install free_proxy_list
+```
+
+## Install the latest version
+
+```bash
+git clone https://github.com/bes-dev/free-proxy-list.python
+cd free-proxy-list.python
+pip install .
+```
+
+## Example
+
+```python
+from free_proxy_list import FreeProxyListClient
+
+client = FreeProxyListClient()
+search_results = client.search(get_random_proxy=True)[0]
+print(search_results)
+search_results = client.search(https='yes', get_random_proxy=True)[0]
+print(search_results)
+search_results = client.search(https='yes', get_random_proxy=True, update_proxies=True)[0]
+print(search_results)
+```
---
+++ setup.py
@@ -0,0 +1,52 @@
+"""
+Copyright 2024 by Sergei Belousov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from setuptools import setup, find_packages
+
+readme = open('README.md').read()
+
+VERSION = '2024.05.19.0'
+
+requirements = [
+    'requests',
+    'beautifulsoup4'
+]
+
+setup(
+    # Metadata
+    name='free_proxy_list',
+    version=VERSION,
+    author='Sergei Belousov aka BeS',
+    author_email='sergei.o.belousov@gmail.com',
+    description='Simple python client to https://free-proxy-list.net/',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+
+    # Package info
+    packages=find_packages(exclude=('*test*',)),
+
+    #
+    zip_safe=True,
+    install_requires=requirements,
+
+    # Classifiers
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+
+
+    # install .json configs
+    include_package_data=True
+)
